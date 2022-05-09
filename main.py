db928fa269b9ada190342b464e29e2ee2cedafbf
zsszakal <zsszakal@outlook.com>
2022-05-09 12:27:54 +0200

    Add win logic for vertical, horizontal, increasing and decreasing diagonals fours

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[utils.ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
     for r in range(utils.ROW_COUNT):
@@ -19,9 +19,32 @@ def get_next_open_row(board, col):
 def print_board(board):
     print(np.flip(board, 0))
 
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(utils.COLUMN_COUNT-3):
+        for r in range(utils.ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Check vertical locations for win
+    for c in range(utils.COLUMN_COUNT):
+        for r in range(utils.ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Check positively sloped diagonals
+    for c in range(utils.COLUMN_COUNT-3):
+        for r in range(utils.ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    # Check negatively sloped diagonals
+    for c in range(utils.COLUMN_COUNT-3):
+        for r in range(3, utils.ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
 board = create_board()
 print_board(board)
-
 game_over = False
 turn = 0
 
@@ -30,17 +53,26 @@ while not game_over:
     if turn == 0:
         col = int(input("Player 1 - make your selection (0-6):"))
 
-        if is_valid_location(board,col):
+        if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("PLAYER 1 WINS!")
+                game_over = True
 
     # Ask for Player 2 input
     else:
         col = int(input("Player 2 - make your selection (0-6):"))
 
-        if is_valid_location(board,col):
+        if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("PLAYER 2 WINS!")
+                game_over = True
+                break
 
     print_board(board)
 
